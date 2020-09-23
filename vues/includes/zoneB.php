d27c0149511a988649788f6890cae2dbc43a8625
DEVLOG.E04 <DEVLOG.E04@afpar.com>
2020-09-23 14:01:32 +0400

    ajout required input

--- vues/includes/zoneB.php
+++ vues/includes/zoneB.php
@@ -7,11 +7,11 @@
     <form id="saisie" action="control/login.php" method="POST">
         <label>Entrer votre email:</label><br />
 
-        <input type="email" id="email" name="email" placeholder="exemple@gmail.com" size="25"><br /><br />
+        <input type="email" id="email" name="email" placeholder="exemple@gmail.com" size="25" required><br /><br />
 
         <label>Mot de passe:</label><br />
 
-        <input type="password" id="pass" name="password" size="25"><br /><br />
+        <input type="password" id="pass" name="password" size="25" required><br /><br />
 
         <input type ="submit" value ="Se connecter"/>
     </form>
